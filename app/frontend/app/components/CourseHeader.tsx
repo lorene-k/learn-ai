3af2f1d85a8a5587e7743e9725b9dae0a908a2dc
Lorene <lorenejulie@hotmail.com>
2025-12-03 18:16:06 +0100

    feat: add icons in CourseHeader

--- app/frontend/app/components/CourseHeader.tsx
+++ app/frontend/app/components/CourseHeader.tsx
@@ -1,6 +1,8 @@
 import StarIcon from '@mui/icons-material/Star';
 import StarBorderIcon from "@mui/icons-material/StarBorder";
 import { Box, Typography } from "@mui/material";
+import TimerOutlinedIcon from '@mui/icons-material/TimerOutlined';
+import SignalCellularAltIcon from '@mui/icons-material/SignalCellularAlt';
 
 interface CourseHeaderProps {
     level: string;
@@ -12,8 +14,14 @@ export function CourseHeader({ level, duration, is_favorite }: CourseHeaderProps
     return (
         <Box sx={{ backgroundColor: "rgb(0, 0, 0, 0.2)", display: "flex", p: 2, borderRadius: 1, justifyContent: "space-between", alignItems: "center" }}>
                         <Box sx={{ display: "flex", flexDirection: "column", gap:1 }}>
-                            <Typography variant="body2"> Level: {level}</Typography>
-                            <Typography variant="body2"> Duration: {duration} minutes</Typography>
+                            <Box sx={{ display: "flex", alignItems: "center", gap: 0.5 }}>
+                                <SignalCellularAltIcon sx={{ fontSize: 20, verticalAlign: "middle", mr: 0.5 }}/>
+                                <Typography variant="body2">Level: {level}</Typography>
+                            </Box>
+                            <Box sx={{ display: "flex", alignItems: "center", gap: 0.5 }}>
+                                <TimerOutlinedIcon sx={{ fontSize: 20, verticalAlign: "middle", mr: 0.5 }} />
+                                <Typography variant="body2"> Duration: {duration} minutes</Typography>
+                            </Box>
                         </Box>
                         {is_favorite ? <StarIcon sx={{ color: "primary.main", fontSize: 30 }} /> :
                             <StarBorderIcon sx={{ color: "gray", fontSize: 30 }} />}
--- app/frontend/app/components/LearnForm.tsx
+++ app/frontend/app/components/LearnForm.tsx
@@ -50,7 +50,7 @@ export function LearnForm() {
         <Box>
             <Box component="form"
                 onSubmit={handleSubmit}
-                sx={{ display: 'flex', flexDirection: 'column', gap: 2, width: '400px', height:'800px', margin: 'auto', mt: 10, p: 3, borderRadius: 2}}
+                sx={{ display: 'flex', flexDirection: 'column', gap: 2, width: '400px', height: '800px', margin: 'auto', mt: 10, p: 3, borderRadius: 2 }}
             >
                 <TextInput name="topic"
                     error={formError}
@@ -66,7 +66,7 @@ export function LearnForm() {
                     value={formDuration}
                     labels={["5", "10", "20", "30"]}
                     onChange={(e) => setDuration(e.target.value)} />
-                <Button type="submit" variant="contained"  sx={{ fontWeight: '500' }}>Learn now</Button>
+                <Button type="submit" variant="contained" sx={{ fontWeight: '500' }}>Learn now</Button>
             </Box>
             {generateError && (
                 <Typography variant="body2" color="error" sx={{ textAlign: 'center', mt: 5 }}>
